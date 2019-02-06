db60b28603becd03f97141e8b76f79dc0d973f97
keziah <k.milligan.1@research.gla.ac.uk>
2019-02-06 15:19:56 +0000

    hough transformer skeleton

--- src/hough.cxx
+++ src/hough.cxx
@@ -1,9 +1,35 @@
+#include <cmath>
+
 #include "hough.h"
 
-HoughTransformer::HoughTransformer()
+HoughTransformer::HoughTransformer(const double thetaStep,
+                                   const double thetaStart,
+                                   const double thetaStop,
+                                   const bool endpoint)
 {
 }
 
 HoughTransformer::~HoughTransformer()
 {
+}
+
+void HoughTransformer::transform(double* inputSignal, std::size_t x_size,
+                                 std::size_t y_size=1)
+{
+    
+}
+
+int HoughTransformer::getRhoLine(std::size_t x, std::size_t y,
+                                 std::size_t theta)
+{
+    double rho = x
+}
+
+void HoughTransformer::quantize(std::size_t max, std::size_t steps)
+{
+}
+
+void HoughTransformer::edges()
+{
+
 }
--- src/hough.h
+++ src/hough.h
@@ -1,13 +1,42 @@
 #ifndef _HOUGH_H_
 #define _HOUGH_H_
 
+#include <cmath>
+
 class HoughTransformer {
 public:
     /*! Make a HoughTransformer
+    *   \param thetaStep Size of angle to step through (radians). Default is
+    *   pi/180 (i.e. 1 degree).
+    *   \param thetaStart Angle at which to start. Default is zero.
+    *   \param thetaStop Angle at which to end. Default is pi.
+    *   \param endpoint If true, thetaStop will be included. If false, angles
+    *    will go up to, but not include, thetaStop. Default is false.
     */
-    HoughTransformer();
+    HoughTransformer(const double thetaStep=M_PI/180., const double thetaStart=0.,
+                     const double thetaStop=M_PI, const bool endpoint=false);
+
+    /*! Get the Hough transform of a signal
+    *   \param inputSignal Input array
+    *   \param x_size Array length
+    *   \param y_size Array height. Default is 1.
+    */
+    void transform(double* inputSignal, std::size_t x_size,
+                   std::size_t y_size=1);
 
     ~HoughTransformer();
+
+protected:
+    /*! Array of x values */
+    std::size_t* x_values;
+    /*! Array of y values */
+    std::size_t* y_values;
+    /*! Calculate rho from equation of straight line */
+    int getRhoLine(std::size_t x, std::size_t y, std::size_t theta);
+    /*! Quantize values */
+    void quantize(std::size_t max, std::size_t steps); // take input and fill y values
+    /*! High pass filter (edge detector) */
+    void edges();
 }
 
 #endif
