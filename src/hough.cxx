b663887b264b332f705e708db4fb8a092ca9edac
keziah <k.milligan.1@research.gla.ac.uk>
2019-02-06 15:23:27 +0000

    added sine and cosine lookup tables

--- src/hough.cxx
+++ src/hough.cxx
@@ -6,7 +6,23 @@ HoughTransformer::HoughTransformer(const double thetaStep,
                                    const double thetaStart,
                                    const double thetaStop,
                                    const bool endpoint)
+    : thetaSize {static_cast<std::size_t>(round((thetaStop-thetaStart)/thetaStep))}
 {
+    // if endpoint was requested, we need one more theta value
+    if (endpoint)
+        thetaSize++;
+
+    // make lookup tables for sin and cos
+    sinLT = new double[thetaSize];
+    cosLT = new double[thetaSize];
+
+    // fill lookup tables
+    double theta = thetaStart;
+    for (std::size_t i{0}; i<thetaSize; i++) {
+        sinLT[i] = sin(theta);
+        cosLT[i] = cos(theta);
+        theta += thetaStep;
+    }
 }
 
 HoughTransformer::~HoughTransformer()
@@ -16,7 +32,7 @@ HoughTransformer::~HoughTransformer()
 void HoughTransformer::transform(double* inputSignal, std::size_t x_size,
                                  std::size_t y_size=1)
 {
-    
+
 }
 
 int HoughTransformer::getRhoLine(std::size_t x, std::size_t y,
